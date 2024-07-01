39b140ec1e85518350525ddd31278c8cb275a9e9
marco-soria <marco.soriaa7@gmail.com>
2024-07-01 15:48:01 -0500

    added backend functionality

--- api/index.js
+++ api/index.js
@@ -1,28 +1,56 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 5100;
+
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log('MongoDb is connected');
+    console.log('MongoDB is connected');
   })
   .catch((err) => {
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
+const app = express();
+const port = process.env.PORT || 5100;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
       }
   );
 
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
-//   const port = process.env.PORT || 5100;
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
