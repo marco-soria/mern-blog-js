8efb3e72515e5e3ba107dddda8d6fa17c7978918
marco-soria <marco.soriaa7@gmail.com>
2024-07-03 15:53:15 -0500

    config for deploy

--- api/index.js
+++ api/index.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
 
 dotenv.config();
 
-
-
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -22,35 +23,40 @@ mongoose
   });
 
 const __dirname = path.resolve();
-
 const app = express();
 const port = process.env.PORT || 5100;
 
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(helmet());
+app.use(morgan('tiny'));
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-      }
-  );
+// Static Files
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
+// Routes
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
+// SPA Support
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
+// Error Handling Middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
   res.status(statusCode).json({
     success: false,
     statusCode,
-    message,
+    message: err.message || 'Internal Server Error',
   });
+});
+
+// Server Listening
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
